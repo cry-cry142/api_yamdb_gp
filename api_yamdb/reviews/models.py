d5d0a98135844e01fa075b2432263b604251ac02
Кирилл Касаткин <cry-cry142@yandex.ru>
2023-05-31 01:13:55 +0300

    Created Comment model.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    review_id = models.ForeignKey(
+        'Review',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True
+    )
