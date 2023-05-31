e981c93866e75d4cbd22589f3b18148a662ea0a3
Кирилл Касаткин <cry-cry142@yandex.ru>
2023-05-31 17:15:00 +0300

    Add model "Review"

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,44 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
+class Review(models.Model):
+    title = models.ForeignKey(
+        'Title',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.IntegerField(
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ]
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='unique_title_author_rewiew'
+            ),
+        ]
+
+    def __str__(self):
+        return self.text
+
+
 class Comment(models.Model):
-    review_id = models.ForeignKey(
+    review = models.ForeignKey(
         'Review',
         on_delete=models.CASCADE,
         related_name='comments'
@@ -17,3 +53,6 @@ class Comment(models.Model):
         auto_now_add=True,
         db_index=True
     )
+
+    def __str__(self):
+        return self.text
