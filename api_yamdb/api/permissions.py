18f090c023dad571bd8f852c72273ffa3a9718b6
Ivan Pavlov <divaythfyr@mail.ru>
2023-06-02 20:46:55 +0300

    Add custom permissions

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,35 @@
+from rest_framework import permissions
+
+ADD_METHODS = ('POST',)
+EDIT_METHODS = ('PUT', 'PATCH', 'DELETE')
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in permissions.SAFE_METHODS
+            or request.user
+            and request.user.is_staff
+        )
+
+
+class IsResponsibleUserOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.method in permissions.SAFE_METHODS
+            or (
+                request.method in ADD_METHODS
+                and request.user
+                and request.user.is_authenticated
+            ) or (
+                request.method in EDIT_METHODS
+                and request.user
+                and (
+                    obj.author == request.user
+                    or request.user.is_staff
+                    or request.user.is_moderator
+                )
+            )
+        )
