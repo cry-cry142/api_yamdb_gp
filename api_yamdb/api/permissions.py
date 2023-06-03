790d946b66257e54d6386535fe861437b896d79f
Ivan Pavlov <divaythfyr@mail.ru>
2023-06-03 20:21:03 +0300

    Fix logic

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -10,26 +10,29 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         return bool(
             request.method in permissions.SAFE_METHODS
             or request.user
+            and request.user.is_authenticated
             and request.user.is_staff
         )
 
 
 class IsResponsibleUserOrReadOnly(permissions.BasePermission):
 
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         return bool(
             request.method in permissions.SAFE_METHODS
             or (
-                request.method in ADD_METHODS
+                request.method in (ADD_METHODS + EDIT_METHODS)
                 and request.user
                 and request.user.is_authenticated
-            ) or (
-                request.method in EDIT_METHODS
-                and request.user
-                and (
-                    obj.author == request.user
-                    or request.user.is_moderator
-                    or request.user.is_staff
-                )
+            )
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.method not in EDIT_METHODS
+            or (
+                obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_staff
             )
         )
