67907a5f5aa04e108e8a71a351acbe4d784e9783
Кирилл Касаткин <cry-cry142@yandex.ru>
2023-06-03 14:37:54 +0300

    beta version of views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import ModelViewSet
+# from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from reviews.models import Title, Review
+from .serializers import ReviewSerializer, CommentSerializer
+
+
+class ReviewViewSet(ModelViewSet):
+    http_method_names = [
+        'get', 'post', 'patch', 'delete', 'head', 'options', 'trace'
+    ]
+    serializer_class = ReviewSerializer
+    # pagination_class = LimitOffsetPagination
+
+    # Необходимо поменять пермишен.
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        queryset = title.reviews.select_related('author').all()
+        return queryset
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(
+            author=self.request.user,
+            title=title
+        )
+
+
+class CommentViewSet(ModelViewSet):
+    http_method_names = [
+        'get', 'post', 'patch', 'delete', 'head', 'options', 'trace'
+    ]
+    serializer_class = CommentSerializer
+    # pagination_class = LimitOffsetPagination
+
+    # Необходимо поменять пермишен.
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id, title=title_id)
+        queryset = review.comments.select_related('author').all()
+        return queryset
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id, title=title_id)
+        serializer.save(
+            author=self.request.user,
+            review=review
+        )
