ee6d187db80d62854a8da6e73333d92f516b5722
Ivan Pavlov <divaythfyr@mail.ru>
2023-06-03 18:20:33 +0300

    Add signup view

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from rest_framework import permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import SignUpSerializer
+
+User = get_user_model()
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def signup(request):
+    serializer = SignUpSerializer(data=request.data)
+    if serializer.is_valid():
+        user, _ = User.objects.get_or_create(**serializer.validated_data)
+        confirmation_code = default_token_generator.make_token(user)
+        send_mail(
+            subject='Регистрация на сервисе YamDB прошла успешно!',
+            message=f'Ваш персональный код подтверждения: {confirmation_code}',
+            recipient_list=[user.email],
+            fail_silently=False
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEFAULT_FROM_EMAIL = 'YaMDB@yandex.ru'
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
