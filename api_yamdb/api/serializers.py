edebff347490c58ffd60ecdc840ab2a5b0746c59
Кирилл Касаткин <cry-cry142@yandex.ru>
2023-06-04 14:44:42 +0300

    fix fields "author"

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -7,12 +7,12 @@ from reviews.models import Review, Comment
 
 class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        slug_field='username'
+        slug_field='username', read_only=True
     )
 
     class Meta:
         fields = ('id', 'text', 'author', 'score', 'pub_date')
-        read_only_fields = ('id', 'author', 'pub_date')
+        read_only_fields = ('id', 'pub_date')
         model = Review
 
         validators = [
@@ -30,10 +30,10 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        slug_field='username'
+        slug_field='username', read_only=True
     )
 
     class Meta:
         fields = ('id', 'text', 'author', 'pub_date')
-        read_only_fields = ('id', 'author', 'pub_date')
+        read_only_fields = ('id', 'pub_date')
         model = Comment
