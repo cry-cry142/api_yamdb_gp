09e6bdf8809b48ed958a360f300c33745d0559dc
Кирилл Касаткин <cry-cry142@yandex.ru>
2023-06-04 14:49:41 +0300

    Add urls for Reviews, Comments

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+from .views import ReviewViewSet, CommentViewSet
+
+app_name = 'api'
+
+router = SimpleRouter()
+router.register(r'titles/(?P<title_id>[\d]+)/reviews',
+                ReviewViewSet, basename='reviews')
+router.register(r'titles/(?P<title_id>[\d]+)/reviews/(?P<review_id>[\d]+)',
+                CommentViewSet, basename='comments')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
