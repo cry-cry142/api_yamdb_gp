ed1b223226124c50578014f547ccda341ce36a3f
Ivan Pavlov <divaythfyr@mail.ru>
2023-06-04 23:52:49 +0300

    New fix

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -15,21 +15,11 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         )
 
 
-class IsResponsibleUserOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return bool(
-            request.method in permissions.SAFE_METHODS
-            or (
-                request.method in (ADD_METHODS + EDIT_METHODS)
-                and request.user
-                and request.user.is_authenticated
-            )
-        )
+class IsResponsibleUserOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
 
     def has_object_permission(self, request, view, obj):
         return bool(
-            request.method not in EDIT_METHODS
+            request.method in permissions.SAFE_METHODS
             or (
                 obj.author == request.user
                 or request.user.is_moderator
