bcd489f43c1b52e36bfe779a4040fa9a90134747
Кирилл Касаткин <cry-cry142@yandex.ru>
2023-06-10 22:01:39 +0300

    Fix

--- api_yamdb/api/decorators.py
+++ api_yamdb/api/decorators.py
@@ -3,6 +3,11 @@ from rest_framework.response import Response
 
 
 def not_allowed_put_method(cls):
+    """
+    Декоратор класса.\n
+    Меняет в классе метод update().
+    При PUT запросе выдает ошибку 405_METHOD_NOT_ALLOWED.
+    """
     upd = cls.update
 
     def update(self, request, *args, **kwargs):
